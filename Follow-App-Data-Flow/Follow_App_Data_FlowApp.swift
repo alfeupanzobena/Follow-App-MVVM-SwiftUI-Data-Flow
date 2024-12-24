a6d1138b4c34a3106818caf2260eda2015fe841b
alfeupanzobena <alfeupanzobena28@gmail.com>
2024-12-24 10:09:04 +0100

    Initial Commit

---
+++ Follow-App-Data-Flow/Follow_App_Data_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Follow_App_Data_FlowApp.swift
+//  Follow-App-Data-Flow
+//
+//  Created by Alfeu Panzo Bena on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Follow_App_Data_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
